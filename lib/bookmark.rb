7b49a0412c15a15088fe994b12174954d4362645
William Powell <william.powell92@gmail.com>
2018-05-09 10:00:58 +0100

    Refactored bookmark class

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,16 +1,9 @@
 require 'pg'
 
 class Bookmark
-
   def self.all
-    urls = []
-    con = PG.connect dbname: 'bookmark_manager'
-    rs = con.exec 'SELECT * FROM bookmarks'
-
-    rs.each do |row|
-      urls << row['url']
-    end
-
-    urls
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM bookmarks')
+    result.map { |bookmark| bookmark['url'] }
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -4,7 +4,7 @@ describe Bookmark do
 
   describe '.all' do
     it 'returns bookmarks' do
-      bookmarks = ['http://makersacademy.com', 'http://google.com', 'http://destroyallsoftware.com']
+      bookmarks = ['http://makersacademy.com', 'http://google.com', 'http://destroyallsoftware.com', 'http://facebook.com']
       expect(described_class.all).to eq bookmarks
     end
   end
